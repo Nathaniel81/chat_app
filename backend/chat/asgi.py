b1af3483bdd4488a2fb73bcaa83ab2f96789f395
Nathaniel81 <nathanield356@gmail.com>
2024-07-05 09:56:44 +0300

    nc

--- backend/chat/asgi.py
+++ backend/chat/asgi.py
@@ -16,14 +16,16 @@
 
 import os
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from core.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
